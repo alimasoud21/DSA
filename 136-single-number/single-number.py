fdf0a90b80edbcf4ff100284b04be57c771d9937
علي ابراهيم علي مسعود ناصر <211250420+alimasoud21@users.noreply.github.com>
2026-01-27 20:03:13 +0200

    Time: 3 ms (63.88%) | Memory: 21 MB (35.38%) - LeetSync

---
+++ 136-single-number/single-number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        
+        XOR = 0
+        for num in nums:
+            XOR ^= num
+        return XOR
+    
+            
